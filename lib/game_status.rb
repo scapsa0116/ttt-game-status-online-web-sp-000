08f062ebbdf4c8aeac31fb821c4334feb0b0fa38
Stela Capsa <stela.capsa@gmail.com>
2019-11-01 17:40:42 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,63 @@
-# Helper Method
+require "pry"
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
-# Define your WIN_COMBINATIONS constant
+
+def won?(board)
+#binding.pry
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]# value of board at win_index_1
+    position_2 = board[win_index_2]# value of board at win_index_2
+    position_3 = board[win_index_3]# value of board at win_index_3
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else
+       false
+    end
+  end
+  return false
+end
+
+def full?(board)
+   board.all? {|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  else won?(board)
+   return false
+ end
+end
+
+def over?(board)
+  if draw?(board) || won?(board) || full?(board)
+    return true
+  end
+end
+
+def winner(board)
+  if won?(board)
+      return board[won?(board)[0]]
+   end
+end
