a5355b19c3342a4e4c3afa1a1ab0c9dc921130cb
Stela Capsa <stela.capsa@gmail.com>
2019-11-01 17:39:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -41,7 +41,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won(board) && full?(board)
+  if !won?(board) && full?(board)
     return true
   elsif !won?(board) && !full?(board)
     return false
